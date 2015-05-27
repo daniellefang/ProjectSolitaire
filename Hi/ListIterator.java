7b8d9aae55e25abb048c34e57c6ded07740faf5f
Danielle Fang <daniellefang1@gmail.com>
2015-05-27 09:50:23 -0500

    Wrote in ListIterator

--- Hi/ListIterator.java
+++ Hi/ListIterator.java
@@ -1,33 +1,31 @@
-
-/**
- * Write a description of class ListIterator here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class ListIterator
+class ListIterator
 {
-    // instance variables - replace the example below with your own
-    private int x;
+  private Link currentLink;
+  
+  public ListIterator (Link firstLink)
+  {
+    currentLink = firstLink;
+  }
 
-    /**
-     * Constructor for objects of class ListIterator
-     */
-    public ListIterator()
-    {
-        // initialise instance variables
-        x = 0;
-    }
+  // true if we reached end of list, false otherwise
+  public boolean atEnd()
+  {
+    return currentLink == null;
+  }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+  // move to next link
+  public void next()
+  {
+    if (currentLink != null)
+      currentLink = currentLink.next();
+  }
+
+  // return value of current link
+  public Object current()
+  {
+    if (currentLink == null)
+      return null;
+
+    return currentLink.value();
+  }
 }
